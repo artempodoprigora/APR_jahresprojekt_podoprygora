84e3d35bbc6193a7267d9fb9f34e048556f61cc5
Artem Podoprigora <artem.podoprigora@hak-kitz.ac.at>
2024-01-30 07:51:40 +0100

    +

--- APR_jahresprojekt_podoprygora/form_minigame1.Designer.cs
+++ APR_jahresprojekt_podoprygora/form_minigame1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "form_minigame1";
+            SuspendLayout();
+            // 
+            // form_minigame1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(559, 459);
+            Name = "form_minigame1";
+            Text = "form_minigame1";
+            ResumeLayout(false);
         }
 
         #endregion
